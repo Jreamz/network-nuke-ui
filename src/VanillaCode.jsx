93c55848288a4c25ad867a75ae1231a594065e08
Jreamz <masonputney@gmail.com>
2018-11-10 12:58:30 -0700

    Migrated vanilla code from Flask to React

---
+++ src/VanillaCode.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './vanilla.css'
+
+const VanillaCode = () => {
+  return (
+    <body>
+      <div className="wrapper">
+      <div className="button">
+      <button type="button">Terminate</button></div>
+      <h1 className="title">Wolverine Network Termination</h1>
+      </div>
+    </body>
+  )
+}
+
+export default VanillaCode
